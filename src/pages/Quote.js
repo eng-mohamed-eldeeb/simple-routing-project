8c39da1a89957ce27b632712150727760190cb44
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-08-02 06:34:44 +0200

    make the project more dynamic

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,29 +1,37 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams, useRouteMatch } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Comments from "../components/comments/Comments.js";
-import HighlightedQuote from './../components/quotes/HighlightedQuote';
+import HighlightedQuote from "./../components/quotes/HighlightedQuote";
 const DD = [
   {
     id: "q1",
-    auther: "eldeeb",
+    author: "eldeeb",
     text: "learining react is fun",
   },
   {
     id: "q2",
-    auther: "eldeeb",
+    author: "eldeeb",
     text: "learining react is fuck",
   },
 ];
 const Quote = () => {
+  const match = useRouteMatch();
   const params = useParams();
-  const q = DD.find(quote => quote.id === params.quoteId)
-  if(!q) {
-    return <h1>NO QUOTE FOUND</h1>
+  const q = DD.find((quote) => quote.id === params.quoteId);
+  if (!q) {
+    return <h1>NO QUOTE FOUND</h1>;
   }
   return (
     <>
-      <HighlightedQuote text={q.text} auther={q.auther} />
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <HighlightedQuote text={q.text} author={q.author} />
+      <Route path={`${match.path}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </>
