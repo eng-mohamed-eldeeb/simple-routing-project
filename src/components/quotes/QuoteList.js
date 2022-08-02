4e5ffbd7f1824bd111a3ec641e987dc7a6f544fa
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-08-02 06:36:00 +0200

    use more rout hooks

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,40 @@
 import { Fragment } from 'react';
-
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
+import { useHistory, useLocation } from 'react-router-dom';
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+
+
+  const history = useHistory()
+  const location = useLocation()
+
+  const queryParams = new URLSearchParams(location.search);
+  const  isSortingACS = queryParams.get('sort') === 'asc';
+  
+  const ChangeSortingHandler = () => {
+    history.push('/quotes?sort=' + (isSortingACS? 'des' : 'asc'));
+  }
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingACS)
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={ChangeSortingHandler}>Sort {isSortingACS? 'Desending' : 'Ascending'}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -3,12 +3,12 @@ import QuoteList from "../components/quotes/QuoteList";
 const DD = [
   {
     id: "q1",
-    auther: "eldeeb",
+    author: "eldeeb",
     text: "learining react is fun",
   },
   {
     id: "q2",
-    auther: "eldeeb",
+    author: "eldeeb",
     text: "learining react is fuck",
   },
 ];
